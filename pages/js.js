c401622a56ed52d7f3fddbfeb5a04dda475e5b1f
naveenshukla8102 <51599613+naveenshukla8102@users.noreply.github.com>
2023-03-21 08:20:15 -0700

    my website

---
+++ pages/js.js
@@ -0,0 +1,23 @@
+
+
+// var jay = document.getElementById('jay');
+// // jay.textContent = 'jay';
+// // jay.innerText = "hiii"
+// // jay.style.color = 'red';
+// console.log(jay);
+function getclintdata(){
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var phone = document.getElementById("phone").value;
+var message = document.getElementById("message").value;
+// console.log(name);
+ var responce = document.getElementById('responce');
+ responce.innerText = "thank you " + name + " we hope you be satisfied with us";
+ responce.style.paddingTop = '16vh';
+ responce.style.fontSize = '10vh';
+ responce.classList.add("vh-60",)
+
+
+ var form = document.getElementById('form');
+ form.style.display = 'none';
+}
